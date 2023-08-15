f307547b1868021d1c3fa4d7320d3ac42c7c5615
Francisco Villalba <villalbaportillaf.fv@gmail.com>
2023-08-15 18:03:14 -0500

    implementacion de tiempos

--- 13-SimuladorSemaforo/semaforo.js
+++ 13-SimuladorSemaforo/semaforo.js
@@ -1,14 +1,25 @@
+console.time('Tiempo de ejecución');
+
 const lucesCirculo = document.querySelectorAll('.lucesCirculo');
 let cont = 0;
-const mostrarLuz =()=>{
+const tiempoEjecucionLabel = document.getElementById('tiempoEjecucion'); // Obtén el elemento span por su id
+
+const mostrarLuz = () => {
     lucesCirculo[cont].className = 'lucesCirculo';
     cont++;
 
-    if(cont>2){
+    if (cont > 2) {
         cont = 0;
     }
 
     const luzActual = lucesCirculo[cont];
-    luzActual.classList.add(luzActual.getAttribute('color'))
-}
-setInterval(mostrarLuz,1000)
+    luzActual.classList.add(luzActual.getAttribute('color'));
+
+    // Calcula y muestra el tiempo de ejecución actual
+    const tiempoTranscurrido = Math.floor(performance.now()); // Milisegundos
+    tiempoEjecucionLabel.textContent = `Tiempo de ejecución: ${tiempoTranscurrido} ms`;
+};
+
+setInterval(mostrarLuz, 1000);
+
+console.timeEnd('Tiempo de ejecución');
